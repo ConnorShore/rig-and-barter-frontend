0d93e6d65e1ddd723e2f5cf261ce2f8fc4d1ed43
Connor Shore <cjshore12@gmail.com>
2025-01-27 13:25:04 -0700

    working

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -2,14 +2,15 @@ import { PassedInitialConfig } from 'angular-auth-oidc-client';
 
 export const authConfig: PassedInitialConfig = {
   config: {
-    authority: 'http://localhost:8080/realms/rig-and-barter-realm',
+    authority: 'http://keycloak.default.svc.cluster.local:8080/realms/rig-and-barter-realm', // get same thing as local host
+    // authority: 'http://localhost:8080/realms/rig-and-barter-realm', // we can register and login but jwt token does not work, gets 401 everywhere
     redirectUrl: window.location.origin,
     postLogoutRedirectUri: window.location.origin,
     clientId: 'angular-client',
     scope: 'openid profile offline_access',
     responseType: 'code',
-    // silentRenew: true,
-    // useRefreshToken: true,
-    // renewTimeBeforeTokenExpiresInSeconds: 30
+    silentRenew: true,
+    useRefreshToken: true,
+    renewTimeBeforeTokenExpiresInSeconds: 30
   }
 }
