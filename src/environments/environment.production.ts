dd51c14c61e05fe5bf6aa3080cf80efa45c71560
Connor Shore <cjshore12@gmail.com>
2025-02-17 13:16:52 -0700

    test new access method

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     stripeApiKey: 'pk_test_51PpxCgFAGs69VCFAMJIgF2k6nUPrKQO65ajuNAjXThF9Hq4qNag8m3OujwForQDGM1zVQFd3ZFrEyxxQSzxrL3K100o9PGB7Ui',
     // apiGateway: 'http://localhost:9000',
-    apiGateway: 'http://api-gateway.default.svc.cluster.local:9000',
+    apiGateway: 'http://api-gateway.default.svc.cluster.local',
     keycloakHost: 'http://keycloak.default.svc.cluster.local:8080',
 };
