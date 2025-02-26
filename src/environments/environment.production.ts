331d3d061e216527ed075c1492fbaec1288dfc8c
Connor Shore <cjshore12@gmail.com>
2025-02-26 14:31:30 -0700

    new endpoint for keycloak

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,7 +1,8 @@
 export const environment = {
     production: true,
     stripeApiKey: 'pk_test_51PpxCgFAGs69VCFAMJIgF2k6nUPrKQO65ajuNAjXThF9Hq4qNag8m3OujwForQDGM1zVQFd3ZFrEyxxQSzxrL3K100o9PGB7Ui',
-    // apiGateway: '',
     apiGateway: 'https://pc-rig-and-barter.com',
-    keycloakHost: 'http://keycloak.default.svc.cluster.local:8080',
+    keycloakHost: 'https://pc-rig-and-barter.com'
+    // apiGateway: '',
+    // keycloakHost: 'http://keycloak.default.svc.cluster.local:8080',
 };
