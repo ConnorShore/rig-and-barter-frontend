6679b147a69a607ca312a1534b228c23e35cea85
Connor Shore <cjshore12@gmail.com>
2025-03-05 23:28:58 -0700

    need the environment.ts file

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    stripeApiKey: '__STRIPE_API_KEY__',
+    apiGateway: 'http://localhost:9000',
+    keycloakHost: 'http://localhost:8180',
+};
